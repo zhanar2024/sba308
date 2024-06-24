92dbb5007fc83c726fe6ca10146358e7d2603f63
zhanar2024 <160571337+zhanar2024@users.noreply.github.com>
2024-06-24 18:30:48 -0400

    added iced coffe selection and some css

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
 // SBA 308
 
-//const result = document.getElementById("result");
-//const title = document.getElementById("title");
-
 //retrieving hot coffee types
 showHotCoffee();
 async function showHotCoffee() {
   const resp = await fetch(`https://api.sampleapis.com/coffee/hot`);
   const data = await resp.json();
   console.log(data);
+  let output = ``;
+  result.innerHtml = "";
+  title.innerText = "";
+  data.forEach((each) => {
+    output += `<li>${each.title}</li>`;
+  });
+  title.innerText = "HOT coffee selection";
+  result.innerHTML = output;
+}
+
+//retrieving iced coffee types
+
+showIcedCoffee();
+async function showIcedCoffee() {
+  const resp = await fetch(`https://api.sampleapis.com/coffee/iced`);
+  const data = await resp.json();
+  console.log(data);
+  let output = ``;
+  result.innerHtml = "";
+  title.innerText = "";
+  data.forEach((each) => {
+    output += `<li>${each.title}</li>`;
+  });
+  title.innerText = "ICED coffee selection";
+  result.innerHTML = output;
 }
